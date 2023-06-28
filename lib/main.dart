554f333e9a369cf9a8a6aae85fde29710ac2e5e5
Jom Dollesin <codewithjom@gmail.com>
2023-06-28 14:00:52 +0800

    feat: added a navigation bar.

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+          child: Row(
+        children: [
+          Spacer(),
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  currentPage = 0;
+                });
+              },
+              icon: Icon(
+                Icons.home,
+                color: currentPage == 0
+                    ? Color.fromRGBO(203, 73, 101, 1)
+                    : Color.fromRGBO(40, 40, 40, 1),
+              )),
+          Spacer(),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                currentPage = 1;
+              });
+            },
+            icon: Icon(
+              Icons.search,
+              color: currentPage == 1
+                  ? Color.fromRGBO(203, 73, 101, 1)
+                  : Color.fromRGBO(40, 40, 40, 1),
+            ),
+          ),
+          Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.ondemand_video),
+          ),
+          Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.card_travel),
+          ),
+          Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.person),
+          )
+        ],
+      )),
+    );
   }
 }
